ad49024ad27ac8a9424049b52d94cde76e4ce2c1
Inlacou <inlacou@gmail.com>
2016-11-22 13:31:10 +0100

    Cleaned implementation.

--- app/src/main/java/libraries/inlacou/com/button3d/MainActivity.java
+++ app/src/main/java/libraries/inlacou/com/button3d/MainActivity.java
@@ -2,12 +2,35 @@ package libraries.inlacou.com.button3d;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import libraries.inlacou.com.threedimensionalbutton.ThreeDimensionalButton;
 
 public class MainActivity extends AppCompatActivity {
+
+	ThreeDimensionalButton match, wrap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		match = (ThreeDimensionalButton) findViewById(R.id.match);
+		wrap = (ThreeDimensionalButton) findViewById(R.id.wrap);
+
+		match.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "MATCH", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		wrap.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Toast.makeText(MainActivity.this, "WRAP", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
--- threedimensionalbutton/src/main/java/libraries/inlacou/com/threedimensionalbutton/ThreeDimensionalButton_match.java
+++ threedimensionalbutton/src/main/java/libraries/inlacou/com/threedimensionalbutton/ThreeDimensionalButton_match.java
@@ -1,77 +1,44 @@
 package libraries.inlacou.com.threedimensionalbutton;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.content.res.TypedArray;
-import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 /**
  * Created by inlacoubyv on (6/11/15) 13/09/2016.
  */
-public class ThreeDimensionalButton_match extends FrameLayout {
+public class ThreeDimensionalButton_match extends ThreeDimensionalButton {
 
-	private static final String DEBUG_TAG = ThreeDimensionalButton_match.class.getName();
-	private int position;
-	private Callbacks mCallback;
+	private static final String DEBUG_TAG = ThreeDimensionalButton_wrap.class.getName();
 	private Context context;
-
-	private View surfaceLayout, holder;
-	private TextView button;
-	private Rect rect;
 
 	public ThreeDimensionalButton_match(Context context) {
 		super(context);
-		init();
+		initialize();
+		super.init();
 	}
 
 	public ThreeDimensionalButton_match(Context context, Callbacks callbacks) {
-		super(context);
+		super(context, callbacks);
 		this.context = context;
-		mCallback = callbacks;
-		init();
+		initialize();
+		super.init();
 	}
 
 	public ThreeDimensionalButton_match(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
-		init();
-		readAttrs(attrs);
+		initialize();
+		super.init();
+		super.readAttrs(attrs);
 	}
 
 	public ThreeDimensionalButton_match(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		this.context = context;
-		init();
-		readAttrs(attrs);
-	}
-
-	public void setCallback(Callbacks mCallback) {
-		this.mCallback = mCallback;
-	}
-
-	private void init(){
-		getData();
 		initialize();
-		setListeners();
-	}
-
-	public void getData(){
-		if(mCallback!=null) {
-		}
-	}
-
-	public void initialize(View view) {
-		surfaceLayout = view.findViewById(R.id.view_base_layout_surface);
-		button = (TextView) view.findViewById(R.id.button);
-		holder = view.findViewById(R.id.holder);
+		super.init();
+		super.readAttrs(attrs);
 	}
 
 	private void initialize() {
@@ -79,126 +46,9 @@ public class ThreeDimensionalButton_match extends FrameLayout {
 		initialize(rootView);
 	}
 
-	private void readAttrs(AttributeSet attrs) {
-		TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.ThreeDimensionalButton_match, 0, 0);
-		try {
-			//(ta.getInt(R.styleable.ThreeDimensionalButton_color, limit));
-			if(ta.hasValue(R.styleable.ThreeDimensionalButton_match_text)) setText(ta.getString(R.styleable.ThreeDimensionalButton_match_text));
-			if(ta.hasValue(R.styleable.ThreeDimensionalButton_match_textColor)) setTextColor(ta.getColorStateList(R.styleable.ThreeDimensionalButton_match_textColor));
-			if(ta.hasValue(R.styleable.ThreeDimensionalButton_match_textSize)) setTextSize(ta.getDimensionPixelSize(R.styleable.ThreeDimensionalButton_match_textSize, -1));
-		} finally {
-			ta.recycle();
-		}
-	}
-
-	private void setTextSize(float size) {
-		if(size>0) button.setTextSize(ViewUtils.pixelsToSp(context, size));
-	}
-
-	private void setTextColor(ColorStateList color) {
-		button.setTextColor(color);
-	}
-
-	private void setText(String string) {
-		if(string!=null && !string.isEmpty()) button.setText(string);
-	}
-
-	public void populate() {
-	}
-
-	public void setOnClickListener(OnClickListener onClickListener){
-		button.setOnClickListener(onClickListener);
-	}
-
-	private void setListeners() {
-		button.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if(mCallback!=null) mCallback.onSurfaceClick();
-			}
-		});
-		button.setOnTouchListener(new OnTouchListener() {
-			boolean inside, outside;
-
-			@Override
-			public boolean onTouch(View view, MotionEvent motionEvent) {
-				//Log.d(DEBUG_TAG, view.getX() + ", " + view.getY() + " | " + motionEvent.getRawX() + ", " + motionEvent.getRawY());
-				if(motionEvent.getAction()==MotionEvent.ACTION_DOWN) {
-					onEnter(view, motionEvent);
-				} else if(motionEvent.getAction()==MotionEvent.ACTION_UP) {
-					onExit(view, motionEvent);
-				} else if(motionEvent.getAction()==MotionEvent.ACTION_MOVE) {
-					if(!rect.contains(view.getLeft() + (int) motionEvent.getX(), view.getTop() + (int) motionEvent.getY())){
-						// User moved outside bounds
-						onExit(view, motionEvent);
-					}else{
-						// User moved inside bounds
-						onEnter(view, motionEvent);
-					}
-				}
-				return false;
-			}
-
-			private void onEnter(View view, MotionEvent motionEvent) {
-				if(!inside){
-					inside = true;
-					outside = false;
-				}else{
-					return;
-				}
-				final float scale = getContext().getResources().getDisplayMetrics().density;
-				RelativeLayout.LayoutParams paramsSpacer = (RelativeLayout.LayoutParams) holder.getLayoutParams();
-				paramsSpacer.height = (int) (8 * scale + 0.5f);
-				holder.setLayoutParams(paramsSpacer);
-				rect = new Rect(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
-				button.setBackgroundResource(R.drawable.threedimensionalbutton_rectangle_bordered_grey_dark);
-			}
-
-			private void onExit(View view, MotionEvent motionEvent) {
-				if(!outside){
-					outside = true;
-					inside = false;
-				}else{
-					return;
-				}
-				RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams) holder.getLayoutParams();
-				params2.height = (0);
-				holder.setLayoutParams(params2);
-				button.setBackgroundResource(R.drawable.threedimensionalbutton_rectangle_bordered_grey_dark_shadowed);
-			}
-		});
-		button.requestLayout();
-	}
-
-	public void inAnimation(){
-		Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_in);
-		surfaceLayout.startAnimation(animation);
-	}
-
-	public void outAnimation(){
-		Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_out);
-		surfaceLayout.startAnimation(animation);
-		animation.setAnimationListener(new Animation.AnimationListener() {
-			@Override
-			public void onAnimationStart(Animation animation) {
-
-			}
-
-			@Override
-			public void onAnimationEnd(Animation animation) {
-				mCallback.onDelete();
-			}
-
-			@Override
-			public void onAnimationRepeat(Animation animation) {
-
-			}
-		});
-	}
-
-	public interface Callbacks {
-		void onSurfaceClick();
-		void onDelete();
+	@Override
+	public void setOnClickListener(OnClickListener onClickListener) {
+		super.setOnClickListener(onClickListener);
 	}
 
 }
